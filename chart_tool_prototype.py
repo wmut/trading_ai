2d5c1cff7188e270d7a782c3e1d0e9e10156074b
outofoffice <ken.lichtner@gmail.com>
2025-04-01 01:31:48 +0200

    Fetch stock data by selecting data point and changed figure to candlestick.

--- chart_tool_prototype.py
+++ chart_tool_prototype.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plotly_express as px
 import itertools
+import json as json
 
 # Not used
 def display_newtab(n_clicks):
@@ -27,19 +28,20 @@ def create_ticker_data(tab, ticker):
         ticker = 'XRP-USD'
 
     # Fetch historical data starting from January 1, 2020
-    btc_data = yf.download(ticker, start='2025-01-01', end='2025-03-06', interval='1h')
-
-    # prepare dataframe of long format
-    df = btc_data["Close"]
-    df["date"] = btc_data.index
-    print(df.head())
+    df = yf.download(ticker, start='2025-01-01', end='2025-03-06', interval='1h')
 
     return df
 
 
+#todo fetch from web2.0
+def create_resistance_levels(ticker):
+    for resistance_level in resistance_levels:
+        fig.add_hline(y=resistance_level)
+
+
 def create_ticker_chart(df, ticker):
     # Some plotly magic
-    fig = px.line(df, x="date", y=ticker, title=ticker)
+    fig = go.Figure(data=[go.Candlestick(x=df.index, open=list(itertools.chain(*df.Open.values)), high=list(itertools.chain(*df.High.values)), low=list(itertools.chain(*df.Low.values)), close=list(itertools.chain(*df.Close.values)))])
 
     drawn_shapes_format1 = {"drawdirection": "vertical",
                            "color": "blue",
@@ -60,7 +62,11 @@ def create_ticker_chart(df, ticker):
                       yaxis=dict(fixedrange=True),
                       xaxis=dict(fixedrange=True))
 
-    menus = list([dict(type='buttons', buttons=list([dict(label='marker',
+    menus = list([dict(type='buttons', buttons=list([dict(label='select',
+                                                        method='relayout',
+                                                        args=[{'dragmode':'select', }]
+                                                        ),
+                                                     dict(label='marker',
                                                           method='relayout',
                                                           args=[{'dragmode':'drawline', 'newshape': drawn_shapes_format1}]),
                                                      dict(label='line',
@@ -76,31 +82,34 @@ def create_ticker_chart(df, ticker):
                   ])
     fig.update_layout(updatemenus=menus)
 
-    fig.update_traces(marker_size=20)
+    #fig.update_traces(marker_size=20)
 
-    #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    if ticker == "BTC-USD":
+        support_levels = [84500, 85188, 86400]
+        resistance_levels = [87341, 88000, 90907]
 
-    #app = Dash(__name__, external_stylesheets=external_stylesheets)
+    elif ticker == "XRP-USD":
+        support_levels = [1.77, 2.05, 2.08]
+        resistance_levels = [2.15, 2.20, 2.46]
+
+    for resistance_level in resistance_levels:
+        fig.add_hline(y=resistance_level, fillcolor='black', opacity = 0.9)
+
+    for support_level in support_levels:
+        fig.add_hline(y=support_level, fillcolor='black', opacity = 0.9)
 
     return fig
 
+clicked = []
 df1 = create_ticker_data('tab-0', 'BTC-USD')
 df2 = create_ticker_data('tab-1', 'XRP-USD')
 
 # App Layout
-css = ["https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css", ]
-app = Dash(name="AI Trading Dashboard", external_stylesheets=css)
+#css = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#app = Dash(__name__, external_stylesheets=css)
 
-#dcc.Graph(figure=px.line([0,1], title='BTC-USD'), id='my-graph')
-#html.Div([dashboard,
-#html.Div(
-#dcc.Input(id='AgentID', type='text')),
-#html.Button('Submit', id='button'),
-# dcc.Tabs(id='tabs-example', value='tab-1-example',
-# children=[
-# dcc.Tab(label='Tab1', value='tab-1'),
-# dcc.Tab(label='Tab2', value='tab-2')
-# ]),#],html.Div(id='output-tab')),
+css = ["https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css", ]
+app = Dash(name=__name__, external_stylesheets=css)
 
 styles = {
     'pre': {
@@ -109,30 +118,29 @@ styles = {
     }
 }
 
-
-
 app.layout = [
     html.Div([
     html.H1("Chart Dashboard", className = "text-center fw-bold m-2"),
     html.Br(),
 
     dcc.Tabs([
-        dcc.Tab([html.Div(children='Yahoo finance data from 2025-01-01 to 2025-03-06'),
-            dcc.Graph(figure=create_ticker_chart(df1, 'BTC-USD'), id='btc-graph'),
-            html.Div([
-                    dcc.Markdown("""
-                        **Zoom and Relayout Data**
-
-                        Click and drag on the graph to zoom or click on the zoom
-                        buttons in the graph's menu bar.
-                        Clicking on legend items will also fire
-                        this event.
-                    """),
-                    html.Pre(id='relayout-data', style=styles['pre']),
-                ], className='three columns')], label="BTC"),
         dcc.Tab([html.Br(),
             html.Div(children='Yahoo finance data from 2025-01-01 to 2025-03-06'),
-            dcc.Graph(figure=create_ticker_chart(df2, 'XRP-USD'), id='btc-graph'),
+            dcc.Graph(figure=create_ticker_chart(df1, 'BTC-USD'), id='btc-graph'),
+            html.Div([
+                    html.Div(className='row', children=[
+                        html.Div([
+                            dcc.Markdown("""
+                                **Click Data**
+
+                                Click on points in the graph.
+                            """),
+                            html.Pre(id='click-data', style=styles['pre']),
+                        ], className='three columns'),
+                ])], className='three columns')], label="BTC"),
+        dcc.Tab([html.Br(),
+            html.Div(children='Yahoo finance data from 2025-01-01 to 2025-03-06'),
+            dcc.Graph(figure=create_ticker_chart(df2, 'XRP-USD'), id='xrp-graph'),
             html.Div([
                     dcc.Markdown("""
                         **Zoom and Relayout Data**
@@ -147,6 +155,22 @@ app.layout = [
         ])
     ], style={"background-color": "#D3D3D3"}) #"height": "100vh"
 ]
+
+@app.callback(
+    Output("click-data", "children"),
+    Input("btc-graph", "clickData"),
+)
+def display_click_data(clickData):
+    global clicked
+    clicked.append(clickData)
+    print(clicked)
+    return json.dumps(clicked, indent=2)
+
+#def click(clickData):
+#    if not clickData:
+#        raise dash.exceptions.PreventUpdate
+#    print(clickData)
+    #return json.dumps({k: clickData["points"][0][k] for k in ["x", "y"]})
 
 #@app.callback(
 #    Output('btcchart', 'figure'),
@@ -183,6 +207,4 @@ app.layout = [
 
 # Run the app
 if __name__ == '__main__':
-    #print(list(itertools.chain(*generate_plot().Close.values)))
-    #print(generate_plot().index)
     app.run(debug=True, host='0.0.0.0', port=1234)
